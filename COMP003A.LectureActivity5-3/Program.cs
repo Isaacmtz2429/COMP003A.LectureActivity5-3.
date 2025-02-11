7465439498440197ef356594bf4f6beb79ae9655
Isaac Martinez <isaacmartinez2429@gmail.com>
2025-02-11 15:32:46 -0800

    Add project files.

---
+++ COMP003A.LectureActivity5-3/Program.cs
@@ -0,0 +1,16 @@
+﻿/*
+// Author: Isaac Martinez
+// Course: COMP003A
+// Faculty: Jonathan Cruz
+// Purpose: OOP - Abstraction
+*/
+namespace COMP003A.LectureActivity5_3_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("");
+        }
+    }
+}
