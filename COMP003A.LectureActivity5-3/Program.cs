40125857be7767120264766430c94da829ca7394
Isaac Martinez <isaacmartinez2429@gmail.com>
2025-02-12 15:52:14 -0800

    errors everywhere.

--- COMP003A.LectureActivity5-3/Program.cs
+++ COMP003A.LectureActivity5-3/Program.cs
@@ -26,16 +26,12 @@ namespace COMP003A.LectureActivity5_3_
         {
             Console.WriteLine($"Shape: {Name}");
         }
-        
-        {
-            Console.WriteLine("");
-        }
-    }
-    /// <summary>
-    /// Represents a circle derived from Shape
-    /// </summary>
-    class Circle : Shape
-    {
+    
+       /// <summary>
+       /// Represents a circle derived from Shape
+      /// </summary>
+      class Circle : Shape
+    
         //Auto-implemented property
         public double Radius { get; set; }
 
@@ -57,6 +53,138 @@ namespace COMP003A.LectureActivity5_3_
             // returns the area of the circle
             return Math.PI * Radius * Radius;
         }
+   
+         /// <summary>
+        /// Represents a rectangle derived from Shape
+        /// </summary>
+        class Rectangle : Shape
+    
+        // Auto-implemented properties
+        public double Width { get; set; }
+        public double Height { get; set; }
+
+        /// <summary>
+        /// Constructor for Rectangle 
+        /// </summary>
+        /// <param name="width"></param>
+        /// <param name="height"></param>
+        public Rectangle(double width, double height)
+        {
+            Name = "Rectangle";
+            Width = width;
+            Height = height;
+        }
+
+        /// <summary>
+        ///  Calculate the area of the rectangle
+        /// </summary>
+        public override double CalculateArea()
+        {
+            // returns the area of the rectangle
+            return Width * Height;
+        }
+    
+    /// <summary>
+    /// Defines a contract for drawable objects.
+    /// </summary>
+    interface IDrawable
+    {
+        /// <summary>
+        /// Draws the object.
+        /// </summary>
+        void Draw();
     }
-    ///
+    /// <summary>
+    /// Represents a circle derived from shape and implements IDrawable.
+    /// </summary>
+    class Circle : Shape, IDrawable
+    {
+        //Auto-implemented property
+        public double Radius { get; set; }
+
+        /// <summary>
+        /// Constructor for Circle.
+        /// </summary>
+        /// <param name="radius"></param>
+        public Circle(double radius)
+        {
+            Name = "Circle";
+            Radius = radius;
+        }
+
+        /// <summary>
+        /// Calculates the area of the circle.
+        /// </summary>
+        public override double CalculateArea()
+        {
+            // returns the area of the circle
+            return Math.PI * Radius * Radius;
+        }
+
+        /// <summary>
+        /// Implements Draw() methor for a circle.
+        /// </summary>
+        public void Draw()
+        {
+            Console.WriteLine("Drawing a circle.");
+        }
+    }
+        /// <summary>
+        /// Represents a rectangle derived from Shape and implements IDrawable
+        /// </summary>
+    class Rectangle : Shape, IDrawable
+    {
+            // Auto-implemented properties
+            public double Width { get; set; }
+            public double Height { get; set; }
+
+            /// <summary>
+            /// Constructor for Rectangle
+            /// </summary>
+            /// <param name="width"></param>
+            /// <param name="height"></param>
+            public Rectangle(double width, double height)
+            {
+                Name = "Rectangle";
+                Width = width;
+                Height = height;
+            }
+
+            /// <summary>
+            /// Calculates the area of the rectangle
+            /// </summary>
+            public override double CalculateArea()
+            {
+                // returns the area of the rectangle
+                return (Width * Height);
+            }
+
+            /// <summary>
+            /// Implements Draw () method for a rectangle
+            /// </summary>
+
+            public void Draw()
+            {
+                Console.WriteLine("Drawing a rectangle.");
+            }
+
+
+            static void Main(string[] args)
+            {
+                // Create a circle and a rectangle
+                Shape myCircle = new Circle(5);
+                // Create a rectangle
+                Shape myRectangle = new Rectangle(4, 6);
+
+                // Display information about circle
+                myCircle.DisplayInfo();
+                Console.WriteLine($"Area: {myCircle.CalculateArea()}");
+                ((IDrawable)myCircle).Draw();
+
+                // Display information about the rectangle
+                myRectangle.DisplayInfo();
+                Console.WriteLine($"Area: {myRectangle.CalculateArea()}");
+                ((IDrawable)(myRectangle)).Draw();
+            }
+    }
 }
